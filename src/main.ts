6817036f5f0ba1aed52718e9afb97a0d5a0b09e2
김만규 <vlsual0917@gmail.com>
2026-03-16 13:30:56 +0900

    chore: 프로젝트 초기 설정 및 ConfigModule 환경 구축

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api/v1');
+
+  const PORT = configService.get('PORT');
+
+  try {
+    await app.listen(PORT);
+    console.log(`Server is running PORT on ${PORT}`);
+  } catch (error) {
+    console.log('Error starting server: 포트를 열 수 없습니다.', error);
+  }
 }
 bootstrap();
